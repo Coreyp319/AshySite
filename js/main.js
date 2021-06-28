259d8f4e04084c3f862911707bb33b8be25fb7d5
Corey Preston -X (copresto - MAINSTAY SALIRE LLC at Cisco) <copresto@cisco.com>
2021-06-28 16:24:28 -0700

    updated hero imagery, removed san anselmo, tweaked portfolio removing categories - this needs update

--- js/main.js
+++ js/main.js
@@ -1,149 +1,149 @@
-<!--build:js Ashley-Profesional-Site/js/main-min.js -->
+< !--build: js Ashley - Profesional - Site / js / main - min.js-- >
 // @import "@material/button/mdc-button";
 
 
 
 
 
-$(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){
-		selectedClass = $(this).attr("data-rel");
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300);
+// $(function() {
+// 		var selectedClass = "";
+// 		$(".fil-cat").click(function(){
+// 		selectedClass = $(this).attr("data-rel");
+//      $("#portfolio").fadeTo(100, 0.1);
+// 		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+//     setTimeout(function() {
+//       $("."+selectedClass).fadeIn().addClass('scale-anm');
+//       $("#portfolio").fadeTo(300, 1);
+//     }, 300);
 
-  });
-  
-// GREENSOCK - Extensions
-var treatmentStroke = document.getElementById("treatmentStroke");
+//   });
 
-TweenLite.fromTo(treatmentStroke, 2, {
-  fill:"#FFEC3E",
-  
+// // GREENSOCK - Extensions
+// var treatmentStroke = document.getElementById("treatmentStroke");
 
-},
-{
-  fill:"#763DC6",
-  delay:2,
-  repeat:-1,
+// TweenLite.fromTo(treatmentStroke, 2, {
+//   fill:"#FFEC3E",
 
 
-});
-
-TweenMax.fromTo(".extension-greensock", 4, 
-{
-  delay: 1,
-  scale: 1.5,
-  fillOpacity: 0,
-  // transformOrigin: "center center",
-  
-}, {
-  scale: 1,
-  fillOpacity: 1,
-  ease: Back.easeOut.config(1),
-  delay: 3,
-  repeat:-1,
-  fill:"#FFEC3E"
-});
+// },
+// {
+//   fill:"#763DC6",
+//   delay:2,
+//   repeat:-1,
 
 
+// });
 
-  // GREENSOCK - Treatments section
-  TweenMax.staggerFrom ('.treatment-stroke', 1, {scale: 2, autoAlpha: 0, ease:Power4.easeOut},0.5);  
+// TweenMax.fromTo(".extension-greensock", 4, 
+// {
+//   delay: 1,
+//   scale: 1.5,
+//   fillOpacity: 0,
+//   // transformOrigin: "center center",
 
-  // TweenLite.to(".treatment-stroke", 5, {drawSVG:"0 75%"});
-
-  TweenMax.fromTo(".right-bottom", 3, 
-  {
-    delay:1,
-    scale: 2,
-    fillOpacity: 1,
-  }, {
-
-    scale: 1,
-    fillOpacity: 0,
-    delay: 1,
-    ease: Back.easeOut.config(1.5),
-    repeat:-1,
-    x:-10,
-    fill:"#FFEC3E"
-  });
- 
-
-  TweenMax.fromTo(".left-bottom", 3, 
-  {
-    delay:1,
-x:-5,
-    scale: 2,
-    fillOpacity: 1,
-    // transformOrigin: "center center",
-    
-  }, {
-    scale: 1,
-    fillOpacity: 0,
-    delay: 1,
-    ease: Back.easeOut.config(1.5),
-    repeat:-1,
-    x:10,
-    fill:"#FFEC3E"
-  });
-  // end greensock experimentation 
+// }, {
+//   scale: 1,
+//   fillOpacity: 1,
+//   ease: Back.easeOut.config(1),
+//   delay: 3,
+//   repeat:-1,
+//   fill:"#FFEC3E"
+// });
 
 
 
-// Lazy load
-var bLazy = new Blazy({ 
-    selector: 'img' // all images
-});
+//   // GREENSOCK - Treatments section
+//   TweenMax.staggerFrom ('.treatment-stroke', 1, {scale: 2, autoAlpha: 0, ease:Power4.easeOut},0.5);  
 
+//   // TweenLite.to(".treatment-stroke", 5, {drawSVG:"0 75%"});
 
+//   TweenMax.fromTo(".right-bottom", 3, 
+//   {
+//     delay:1,
+//     scale: 2,
+//     fillOpacity: 1,
+//   }, {
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-//   .click(function(event) {
-//     // On-page links
-//     if (
-//       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')&&location.hostname == this.hostname
-//     ) {
-//       // Figure out element to scroll to
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       // Does a scroll target exist?
-//       if (target.length) {
-//         // Only prevent default if animation is actually gonna happen
-//         event.preventDefault();
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000, function() {
-//           // Callback after animation
-//           // Must change focus!
-//           var $target = $(target);
-//           $target.focus();
-//           if ($target.is(":focus")) { // Checking if the target was focused
-//             return false;
-//           } else {
-//             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//             $target.focus(); // Set focus again
-//           }
-//         });
-//       }
-//     }
+//     scale: 1,
+//     fillOpacity: 0,
+//     delay: 1,
+//     ease: Back.easeOut.config(1.5),
+//     repeat:-1,
+//     x:-10,
+//     fill:"#FFEC3E"
 //   });
 
 
-//PORTFOLIO SELECTION END
+//   TweenMax.fromTo(".left-bottom", 3, 
+//   {
+//     delay:1,
+// x:-5,
+//     scale: 2,
+//     fillOpacity: 1,
+//     // transformOrigin: "center center",
+
+//   }, {
+//     scale: 1,
+//     fillOpacity: 0,
+//     delay: 1,
+//     ease: Back.easeOut.config(1.5),
+//     repeat:-1,
+//     x:10,
+//     fill:"#FFEC3E"
+//   });
+//   // end greensock experimentation 
+
+
+
+// // Lazy load
+// var bLazy = new Blazy({ 
+//     selector: 'img' // all images
+// });
+
+
+
+// // Select all links with hashes
+// $('a[href*="#"]')
+//   // Remove links that don't actually link to anything
+//   .not('[href="#"]')
+//   .not('[href="#0"]')
+// //   .click(function(event) {
+// //     // On-page links
+// //     if (
+// //       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')&&location.hostname == this.hostname
+// //     ) {
+// //       // Figure out element to scroll to
+// //       var target = $(this.hash);
+// //       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+// //       // Does a scroll target exist?
+// //       if (target.length) {
+// //         // Only prevent default if animation is actually gonna happen
+// //         event.preventDefault();
+// //         $('html, body').animate({
+// //           scrollTop: target.offset().top
+// //         }, 1000, function() {
+// //           // Callback after animation
+// //           // Must change focus!
+// //           var $target = $(target);
+// //           $target.focus();
+// //           if ($target.is(":focus")) { // Checking if the target was focused
+// //             return false;
+// //           } else {
+// //             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+// //             $target.focus(); // Set focus again
+// //           }
+// //         });
+// //       }
+// //     }
+// //   });
+
+
+// //PORTFOLIO SELECTION END
 
 
 
 
 
-// SRIPT WAIT FUNCTION END
-});
-<!-- endbuild -->
+// // SRIPT WAIT FUNCTION END
+// });
+< !--endbuild -->
